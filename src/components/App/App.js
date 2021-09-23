3a3a11130f6506ea561bc66e63a6121e9fd2e5f6
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-23 22:08:34 +0300

    feat: create routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="page">
+      <Switch>
+        <Route exact path="/">
+          <h1>«О проекте»</h1>
+        </Route>
+        <Route path="/movies">
+          <h1>«Фильмы»</h1>
+        </Route>
+        <Route path="/saved-movies">
+          <h1>«Сохранённые фильмы»</h1>
+        </Route>
+        <Route path="/profile">
+          <h1>страница с профилем пользователя</h1>
+        </Route>
+        <Route path="/signin">
+          <h1>авторизация</h1>
+        </Route>
+        <Route path="/signup">
+          <h1>регистрация</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/">
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
