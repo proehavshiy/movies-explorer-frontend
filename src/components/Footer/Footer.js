45d66698081b0b23fd369ed7817df81255d3d833
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-28 19:01:20 +0300

    feat: init footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css';
 
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function Main() {
   return (
     <>
       <Header />
       <h1>«О проекте»</h1>
+      <Footer />
     </>
   );
 }
