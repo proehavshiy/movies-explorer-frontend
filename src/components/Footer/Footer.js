da9e51cf15d1ae196d1279b4f3e42398764d628f
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-29 18:26:03 +0300

    feat: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,18 @@ import './Footer.css';
 function Footer() {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+      <div className="footer__content">
+        <p className="footer__copyright">
+          &copy;&nbsp;
+          {new Date().getFullYear()}
+        </p>
+        <div className="footer__links">
+          <a className="footer__link page__link" href="https://practicum.yandex.ru/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+          <a className="footer__link page__link" href="https://github.com/proehavshiy" target="_blank" rel="noreferrer">Github</a>
+          <a className="footer__link page__link" href="https://www.facebook.com/" target="_blank" rel="noreferrer">Facebook</a>
+        </div>
+      </div>
     </div>
   );
 }
