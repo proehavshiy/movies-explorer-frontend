e6eddad1ba2612d438257279cd63c5be5818298c
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-10-14 18:58:18 +0300

    refactor: переписал кнопку Аккаунт

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,7 +36,6 @@ function Header({ isLoggedIn }) {
                 <span></span>
               </button>
               <nav className={`header__menu ${isOpenBurgerMenu && 'header__menu_opened'} `}>
-                {/* <div className="header__menu-content-wrapper"> */}
                 <ul className="header__list header__list_nav">
                   <li>
                     <Link className="header__link header__link_navigation page__link" to="/">Главная</Link>
@@ -48,50 +47,13 @@ function Header({ isLoggedIn }) {
                     <Link className="header__link header__link_navigation page__link" to="/saved-movies">Сохранённые фильмы</Link>
                   </li>
                 </ul>
-                <ul className="header__list">
-                  <li>
-                    <Link className="header__link header__link_account page__link" to="/profile">
-                      <p className="header__link-subtitle">Аккаунт</p>
-                      <button className="header__link header__account-button" type="button" aria-label="кнопка открыть профиль" />
-                    </Link>
-                  </li>
-                </ul>
-                {/* </div> */}
+                <Link className="header__link page__link account-button" to="/profile">
+                  <p className="account-button__subtitle">Аккаунт</p>
+                  <div className="account-button__icon"></div>
+                </Link>
               </nav>
             </>
           )}
-
-          {/* <Switch>
-            <Route exact path="/">
-              <AuthBar />
-            </Route>
-            <Route path={['/movies', '/saved-movies', '/profile']}>
-              <button className={`header__burger ${isOpenBurgerMenu && 'header__burger_opened'}`} onClick={handleBurgerClick} type="button" aria-label="кнопка открыть меню">
-                <span></span>
-              </button>
-              <nav className={`header__menu ${isOpenBurgerMenu && 'header__menu_opened'} `}>
-                <ul className="header__list header__list_nav">
-                  <li>
-                    <Link className="header__link header__link_navigation page__link" to="/">Главная</Link>
-                  </li>
-                  <li>
-                    <Link className="header__link header__link_navigation header__link_navigation_accent  page__link" to="/movies">Фильмы</Link>
-                  </li>
-                  <li>
-                    <Link className="header__link header__link_navigation page__link" to="/saved-movies">Сохранённые фильмы</Link>
-                  </li>
-                </ul>
-                <ul className="header__list">
-                  <li>
-                    <Link className="header__link header__link_account page__link" to="/profile">
-                      <p className="header__link-subtitle">Аккаунт</p>
-                      <button className="header__link header__account-button" type="button" aria-label="кнопка открыть профиль" />
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </Route>
-          </Switch> */}
         </div>
       </div>
     </div>
