4f137aeb2431bf12dbfa7da81734ca93b09bd9ee
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-10-21 19:43:20 +0300

    refactor: адаптироать ErrorPage под лэндскейп мобайл

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -6,13 +6,19 @@ import Button from '../Ui/Button/Button';
 function ErrorPage() {
   return (
     <div className="error">
-      <h2 className="error__heading">404</h2>
-      <p className="error__subtitle">Страница не найдена</p>
-      <Button
-        text="Назад"
-        btnStyle="back"
-        onClick={useHistory().goBack}
-      />
+      <div className="error__content-wrapper">
+        <div className="error__position-wrapper">
+          <h1 className="error__heading">404</h1>
+          <p className="error__subtitle">Страница не найдена</p>
+        </div>
+      </div>
+      <div className="error__nav-wrapper">
+        <Button
+          text="Назад"
+          btnStyle="back"
+          onClick={useHistory().goBack}
+        />
+      </div>
     </div>
   );
 }
