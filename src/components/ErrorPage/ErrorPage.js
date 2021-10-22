1274c5f77bd7bdc70b8ecaf766569e433192a279
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-10-22 14:14:45 +0300

    refactor: добавить пропсы ErrorPage

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './ErrorPage.css';
 import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Button from '../Ui/Button/Button';
 
-function ErrorPage() {
+function ErrorPage({ heading, subHeading, btnText }) {
   return (
     <div className="error">
       <div className="error__content-wrapper">
         <div className="error__position-wrapper">
-          <h1 className="error__heading">404</h1>
-          <p className="error__subtitle">Страница не найдена</p>
+          <h1 className="error__heading">{heading}</h1>
+          <p className="error__subtitle">{subHeading}</p>
         </div>
       </div>
       <div className="error__nav-wrapper">
         <Button
-          text="Назад"
+          text={btnText}
           btnStyle="back"
           onClick={useHistory().goBack}
         />
@@ -22,5 +23,17 @@ function ErrorPage() {
     </div>
   );
 }
+
+ErrorPage.propTypes = {
+  heading: PropTypes.string,
+  subHeading: PropTypes.string,
+  btnText: PropTypes.string,
+};
+
+ErrorPage.defaultProps = {
+  heading: '404',
+  subHeading: 'Страница не найдена',
+  btnText: 'Назад',
+};
 
 export default ErrorPage;
