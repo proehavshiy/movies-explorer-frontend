43fdd1640e66fc80a8fef240c2e5336cef308391
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-10-31 20:54:05 +0300

    feat: добавить и настроить компонент Preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Preloader.css';
+
+const Preloader = () => (
+  <div className="preloader">
+    <div className="preloader__container">
+      <span className="preloader__round" />
+    </div>
+  </div>
+);
+
+export default Preloader;
