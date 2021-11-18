05231d97dcd652a48aa8a84929eb34c8faf1eed7
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-11-18 21:43:08 +0300

    fix: изменить лого на svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer page__animation">
       <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
       <div className="footer__content">
         <p className="footer__copyright">
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Logo.css';
 import { Link } from 'react-router-dom';
-import logo from '../../images/logo.png';
+import logo from '../../images/logo.svg';
 
 function Logo() {
   return (
